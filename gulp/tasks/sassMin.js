c2bc0df84db8add9fe2164ffb1726fadb5fd4cc3
Neil Carpenter <neilanthonycarpenter@gmail.com>
2014-08-26 15:39:08 +0100

    Add combine media queries to sass prob build

--- gulp/tasks/sassMin.js
+++ gulp/tasks/sassMin.js
@@ -1,9 +1,11 @@
-var gulp         = require('gulp');
-var minifyCSS    = require('gulp-minify-css');
-var pkg          = require('../../package.json');
+var gulp      = require('gulp');
+var minifyCSS = require('gulp-minify-css');
+var cmq       = require('gulp-combine-media-queries');
+var pkg       = require('../../package.json');
 
 gulp.task('sassMin', ['sass'], function () {
   return gulp.src(pkg.folders.dest+'/css/main.css')
+  	.pipe(cmq())
     .pipe(minifyCSS())
     .pipe(gulp.dest(pkg.folders.dest+'/css'));
 });
