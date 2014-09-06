eef9524dc7ad49de15514355052885ae8eb5c246
Neil Carpenter <neil.carpenter@unit9.com>
2014-09-06 17:44:05 +0100

    begin adding some static asset versioning

---
+++ gulp/tasks/rev.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var pkg  = require('../../package.json');
+
+gulp.task('rev', ['revAssets'], function () {
+
+	// supplant index from src with manifest, pipe to dest
+
+});
