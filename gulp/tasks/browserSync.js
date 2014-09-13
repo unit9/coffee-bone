86dd1fed0634c063cc715528e186796ff3b85d8e
Silvio Paganini <s2paganini@gmail.com>
2014-09-13 17:25:43 +0100

    simple static route for push state

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -1,11 +1,21 @@
 var browserSync = require('browser-sync');
 var gulp        = require('gulp');
+var fs          = require('fs');
 var pkg         = require('../../package.json')
 
 gulp.task('browserSync', ['build'], function() {
   browserSync.init([pkg.folders.dest+'/**'], {
+
     server: {
-      baseDir: [pkg.folders.src, pkg.folders.dest]
+      baseDir: [pkg.folders.src, pkg.folders.dest],
+      middleware: function (req, res, next) {
+
+        // static route for pushstate
+        var exists = fs.existsSync(process.cwd() + "/" + pkg.folders.dest + req.url);
+        if(req.url == "/" || !exists) req.url = "/index.html";
+
+        next();
+      }
     }
   });
 });
