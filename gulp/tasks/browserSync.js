739ae1de8f7367cd68955ab1356143716635a7f3
Neil Carpenter <neil.carpenter@unit9.com>
2014-11-06 23:14:25 +0000

    semi colon

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -1,7 +1,7 @@
 var browserSync = require('browser-sync');
 var gulp        = require('gulp');
 var fs          = require('fs');
-var pkg         = require('../../package.json')
+var pkg         = require('../../package.json');
 
 gulp.task('browserSync', ['build'], function() {
   browserSync.init([pkg.folders.dest+'/**'], {
