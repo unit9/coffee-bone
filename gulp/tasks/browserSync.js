5aa62df1e59b27cd75dec5c5151a846b2d0e2e35
Neil Carpenter <neilanthonycarpenter@gmail.com>
2015-05-29 16:50:45 +0100

    Fix browsersync - upgrade version

--- gulp/tasks/browserSync.js
+++ gulp/tasks/browserSync.js
@@ -1,11 +1,10 @@
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var gulp        = require('gulp');
 var fs          = require('fs');
 var pkg         = require('../../package.json');
 
 gulp.task('browserSync', ['build'], function() {
-  browserSync.init([pkg.folders.dest+'/**'], {
-
+  browserSync.init({
     server: {
       baseDir: [pkg.folders.src, pkg.folders.dest],
       middleware: function (req, res, next) {
